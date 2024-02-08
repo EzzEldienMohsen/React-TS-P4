677801948d61348f46670ee261449d7964bb86a7
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-08 02:30:46 +0200

    fetching and rendering data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import React from 'react';
+import { get } from './util/http';
+import BlogPosts, { type BlogPost } from './components/BlogPosts';
+import img from './assets/data-fetching.png';
+type RawPost = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = React.useState<BlogPost[]>();
+  React.useEffect(() => {
+    async function dataGetter() {
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )) as RawPost[];
+
+      const blogPosts: BlogPost[] = data.map((post) => {
+        return {
+          id: post.id,
+          title: post.title,
+          text: post.body,
+        };
+      });
+      setFetchedPosts(blogPosts);
+    }
+
+    dataGetter();
+  }, []);
+  let content: React.ReactNode;
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={img} alt="image" />
+      {content}
+    </main>
+  );
 }
 
 export default App;
--- src/util/http.ts
+++ src/util/http.ts
@@ -1,4 +1,8 @@
 export async function get(url: string) {
   const response = await fetch(url);
-  console.log(response);
+  if (!response.ok) {
+    throw new Error('there was an error');
+  }
+  const data = response.json() as unknown;
+  return data;
 }
